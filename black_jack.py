e24a427bbe8c443414710c41ba4f87ec742d2a7d
rajaotearoa-code <raj.aotearoa@gmail.com>
2026-09-06 09:43:27 +1200

    Add deal_card function

--- black_jack.py
+++ black_jack.py
@@ -1,2 +1,9 @@
 # Blackjack Game - Team Project
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def deal_card():
+    """Returns a random card from the deck."""
+    return random.choice(cards)
